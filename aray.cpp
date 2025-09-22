7e6c1dc543856758fa1663c6da66129d8fb50abd
rishabhhh18 <rishabh.mathanecse2024@indoreinstitute.com>
2025-09-22 11:36:28 +0530

    Add files via upload

---
+++ aray.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int a[5] = {20, 40, 60, 10}; 
+    int item_search = 45;
+    int i;
+    
+    for (i = 0; i < 5; i++);
+     if (a[i]== item_search)
+    {
+        
+        {
+            printf("Element found at index %d\n", i);
+            return 0;
+        }
+    }
+   
+        printf("Element not found\n");
+    
+    return 0;
+}
+
